4fb412bca1c50b1bc43daa1c25c86422c6f63f37
DarlingAvila <62193255+DarlingAvila@users.noreply.github.com>
2021-05-13 22:06:47 -0500

    nueva actualizacion añadido maps

--- app/src/main/java/com/darling/mototaxi/activities/client/MapClientActivity.java
+++ app/src/main/java/com/darling/mototaxi/activities/client/MapClientActivity.java
@@ -2,36 +2,63 @@ package com.darling.mototaxi.activities.client;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.darling.mototaxi.R;
 import com.darling.mototaxi.activities.MainActivity;
 import com.darling.mototaxi.providers.AuthProvider;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
-public class MapClientActivity extends AppCompatActivity {
+public class MapClientActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-    Button mButtonLogout;
-    AuthProvider mAuthProvider;
+
+    //Button mButtonLogout;
+    private AuthProvider mAuthProvider;
+    private GoogleMap mMap;
+    private SupportMapFragment mMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_client);
 
-        mButtonLogout = findViewById(R.id.btnLogout);
         mAuthProvider = new AuthProvider();
-        mButtonLogout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mAuthProvider.logout();
-                //al momento de cerrar sesion nos envia al mainactivity
-                Intent intent = new Intent(MapClientActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        });
+        mMapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()== R.id.action_logout){
+            logout();
+
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+    void logout(){
+        mAuthProvider.logout();
+        //al momento de cerrar sesion nos envia al mainactivity
+        Intent intent = new Intent(MapClientActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/darling/mototaxi/activities/driver/MapDriverActivity.java
+++ app/src/main/java/com/darling/mototaxi/activities/driver/MapDriverActivity.java
@@ -1,10 +1,17 @@
 package com.darling.mototaxi.activities.driver;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.darling.mototaxi.R;
+import com.darling.mototaxi.activities.MainActivity;
+import com.darling.mototaxi.includs.MyToolbar;
+import com.darling.mototaxi.providers.AuthProvider;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
@@ -13,11 +20,13 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
 
     private GoogleMap mMap;
     private SupportMapFragment mMapFragment;
+    private AuthProvider mAuthProvider;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_driver);
-
+        MyToolbar.show(this,"Mapa de Mototaxista", false );
+        mAuthProvider = new AuthProvider();
         //instancia a las variables
         mMapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mMapFragment.getMapAsync(this);
@@ -28,5 +37,27 @@ public class MapDriverActivity extends AppCompatActivity implements OnMapReadyCa
         mMap = googleMap;
         mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.driver_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId()== R.id.action_logout){
+            logout();
+
+        }
+        return  super.onOptionsItemSelected(item);
+    }
+    void logout(){
+        mAuthProvider.logout();
+        //al momento de cerrar sesion nos envia al mainactivity
+        Intent intent = new Intent(MapDriverActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
